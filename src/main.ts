5f3a5ff4df49f5ee12dbef73f0dd8c03cf12ace9
Mirete Benoit <ben@mirete.fr>
2022-05-22 11:28:28 +0200

    chore: add .env.example & enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('v1');
+  app.enableCors();
+
+  const logger = new Logger('Bootstrap');
+  const port = process.env.PORT;
+  await app.listen(port);
+  logger.log(`Application successfully started on port ${port}`);
 }
 bootstrap();
