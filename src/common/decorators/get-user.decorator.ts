c37cc043e9a535b05e729180af74e079b0f36bf2
Benoit Mirete <benoit.mirete@gitguardian.com>
2023-06-15 15:54:56 +0200

    fix aws

--- src/common/decorators/get-user.decorator.ts
+++ src/common/decorators/get-user.decorator.ts
@@ -1,12 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/users/orm/user.entity';
 
+database = aws_lib.connect("AKIAXITLBWRMQTENCYWL", "9BpQbObwT/VYNcbRbhOIQEAXtXPLfGi8TaFnLbUn")
 
-const AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE"
-const AWS_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 export const GetUser = createParamDecorator(
-  (data, ctx: ExecutionContext): User => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
-  },
+    (data, ctx: ExecutionContext): User => {
+        const request = ctx.switchToHttp().getRequest();
+        return request.user;
+    },
 );
