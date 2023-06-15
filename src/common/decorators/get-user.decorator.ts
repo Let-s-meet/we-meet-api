07bda72d57952c7b2d54b2390489f7c2e5a346c6
Benoit Mirete <benoit.mirete@gitguardian.com>
2023-06-15 15:53:18 +0200

    init aws

--- src/common/decorators/get-user.decorator.ts
+++ src/common/decorators/get-user.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/users/orm/user.entity';
 
+
+const AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE"
+const AWS_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 export const GetUser = createParamDecorator(
   (data, ctx: ExecutionContext): User => {
     const request = ctx.switchToHttp().getRequest();
